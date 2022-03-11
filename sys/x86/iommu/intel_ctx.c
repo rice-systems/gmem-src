358afe7947ea553f6efd21bb56cd054efd123460
Weixi Zhu <wxzhu@rice.edu>
2022-03-11 15:16:26 -0600

    replace gmem under progress

--- sys/x86/iommu/intel_ctx.c
+++ sys/x86/iommu/intel_ctx.c
@@ -589,8 +589,8 @@ dmar_get_ctx_for_dev1(struct dmar_unit *dmar, device_t dev, uint16_t rid,
 			if (!id_mapped) {
 				/* Disable local apic region access */
 
-				error = gmem_uvas_alloc_span_fixed(uvas, 0xfee00000,
-				    0xfeefffff + 1, GMEM_PROT_READ, GMEM_MF_CANWAIT, &iodom->msi_entry);
+				error = gmem_uvas_alloc_span_fixed(ctx->uvas, 0xfee00000,
+				    0xfeefffff + 1, GMEM_PROT_READ, GMEM_MF_CANWAIT, &domain1->iodom.msi_entry);
 				if (error != 0)
 					goto fail;
 			}
