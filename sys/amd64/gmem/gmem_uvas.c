c4755eaeb50f6f6dad92b9f783df325777cf8c81
Weixi Zhu <wxzhu@rice.edu>
2022-03-14 13:17:44 -0500

    debug vmem crashing

--- sys/amd64/gmem/gmem_uvas.c
+++ sys/amd64/gmem/gmem_uvas.c
@@ -255,7 +255,7 @@ gmem_error_t gmem_uvas_alloc_span_fixed(gmem_uvas_t *uvas,
 	}
 	else if (uvas->allocator == VMEM)
 	{
-		vm_offset_t new_start;
+		// vm_offset_t new_start;
 		// use vmem allocator
 		GMEM_UVAS_LOCK(uvas);
 		printf("VMEM xalloc with start %lx, end %lx\n", start, end);
