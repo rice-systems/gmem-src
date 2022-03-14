b2f5a39ce84d8cb02048fd5a5cf6c4a44c79c5be
Weixi Zhu <wxzhu@rice.edu>
2022-03-13 23:57:43 -0500

    debug

--- sys/amd64/gmem/gmem_uvas.c
+++ sys/amd64/gmem/gmem_uvas.c
@@ -274,6 +274,11 @@ gmem_error_t gmem_uvas_free_span(gmem_uvas_t *uvas, vm_offset_t start,
 	vm_size_t size, gmem_uvas_entry_t *entry)
 {
 	KASSERT(uvas != NULL, "The uvas to allocate entry is NULL!");
+	if (uvas == NULL) {
+		PRINTINFO;
+		printf("[gmem panic] uvas is null\n");
+		return -1;
+	}
 	if (uvas->allocator == RBTREE) {
 		GMEM_UVAS_LOCK(uvas);
 		// use rb-tree allocator
