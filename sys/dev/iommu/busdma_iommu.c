f6edcb6fd8098fc9ecb6e3368422c3d8a220cfaa
Weixi Zhu <wxzhu@rice.edu>
2022-03-13 21:01:26 -0500

    remove unroll_link

--- sys/dev/iommu/busdma_iommu.c
+++ sys/dev/iommu/busdma_iommu.c
@@ -718,7 +718,6 @@ iommu_bus_dmamap_load_something(struct bus_dma_tag_iommu *tag,
 {
 	struct iommu_ctx *ctx;
 	struct iommu_domain *domain;
-	struct iommu_map_entry *entry, *entry1;
 	int error;
 
 	ctx = tag->ctx;
