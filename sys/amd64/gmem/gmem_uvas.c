d9331ffdf0a4198454e1b7b617fe32ccf784d492
Weixi Zhu <wxzhu@rice.edu>
2022-03-14 13:15:59 -0500

    debug vmem crashing

--- sys/amd64/gmem/gmem_uvas.c
+++ sys/amd64/gmem/gmem_uvas.c
@@ -259,8 +259,8 @@ gmem_error_t gmem_uvas_alloc_span_fixed(gmem_uvas_t *uvas,
 		// use vmem allocator
 		GMEM_UVAS_LOCK(uvas);
 		printf("VMEM xalloc with start %lx, end %lx\n", start, end);
-		error = vmem_xalloc(uvas->arena, end - start, 0, 0, 0, start, end, 
-			M_FIRSTFIT | ((flags & GMEM_MF_CANWAIT) != 0 ? M_WAITOK : M_NOWAIT), &new_start);
+		// error = vmem_xalloc(uvas->arena, end - start, 0, 0, 0, start, end, 
+		// 	M_FIRSTFIT | ((flags & GMEM_MF_CANWAIT) != 0 ? M_WAITOK : M_NOWAIT), &new_start);
 		GMEM_UVAS_UNLOCK(uvas);
 		if (start != new_start) {
 			printf("VMEM xalloc failed with start %lx, end %lx, newstart %lx\n", start, end, new_start);
