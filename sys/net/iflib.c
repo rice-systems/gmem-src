6fcc9374d19035fd8b4c86221a3428a197c0481a
Weixi Zhu <wxzhu@rice.edu>
2022-04-20 23:16:33 -0500

    add async unmap mechanism

--- sys/net/iflib.c
+++ sys/net/iflib.c
@@ -1829,13 +1829,16 @@ iflib_txsd_free(if_ctx_t ctx, iflib_txq_t txq, int i)
 	if (txq->ift_sds.ifsd_map != NULL) {
 		bus_dmamap_sync(txq->ift_buf_tag,
 		    txq->ift_sds.ifsd_map[i], BUS_DMASYNC_POSTWRITE);
-		bus_dmamap_unload(txq->ift_buf_tag, txq->ift_sds.ifsd_map[i]);
+		// bus_dmamap_unload(txq->ift_buf_tag, txq->ift_sds.ifsd_map[i]);
+		bus_dmamap_unload_async(txq->ift_buf_tag, txq->ift_sds.ifsd_map[i], NULL, NULL);
 	}
 	if (txq->ift_sds.ifsd_tso_map != NULL) {
 		bus_dmamap_sync(txq->ift_tso_buf_tag,
 		    txq->ift_sds.ifsd_tso_map[i], BUS_DMASYNC_POSTWRITE);
-		bus_dmamap_unload(txq->ift_tso_buf_tag,
-		    txq->ift_sds.ifsd_tso_map[i]);
+		// bus_dmamap_unload(txq->ift_tso_buf_tag,
+		//     txq->ift_sds.ifsd_tso_map[i]);
+		bus_dmamap_unload_async(txq->ift_tso_buf_tag,
+		    txq->ift_sds.ifsd_tso_map[i], NULL, NULL);
 	}
 	m_freem(*mp);
 	DBG_COUNTER_INC(tx_frees);
