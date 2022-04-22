e96069889544369d2f669a707db585a564aa60d4
Weixi Zhu <wxzhu@rice.edu>
2022-04-22 14:51:52 -0500

    perf debug

--- sys/net/iflib.c
+++ sys/net/iflib.c
@@ -3610,7 +3610,7 @@ defrag_failed:
 
 static void mfree_cb(void *args)
 {
-	m_freem((struct mbuf *) args);
+	// m_freem((struct mbuf *) args);
 }
 
 static int free_cnt = 0;
@@ -3654,10 +3654,10 @@ iflib_tx_desc_free(iflib_txq_t txq, int n)
 			}
 			/* XXX we don't support any drivers that batch packets yet */
 			MPASS(m->m_nextpkt == NULL);
-			atomic_add_int(&free_cnt, 1);
-			if (free_cnt % 1000 == 0)
-				printf("[iflib] submitted %d free cb\n", free_cnt);
-			// m_freem(m);
+			// atomic_add_int(&free_cnt, 1);
+			// if (free_cnt % 1000 == 0)
+			// 	printf("[iflib] submitted %d free cb\n", free_cnt);
+			m_freem(m);
 			ifsd_m[cidx] = NULL;
 #if MEMORY_LOGGING
 			txq->ift_dequeued++;
