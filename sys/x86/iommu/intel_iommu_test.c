894ba4e2942c1999f07d44ebeb383a917d2da071
Weixi Zhu <wxzhu@rice.edu>
2022-05-04 19:14:42 -0500

    benchmark map cost

--- sys/x86/iommu/intel_iommu_test.c
+++ sys/x86/iommu/intel_iommu_test.c
@@ -68,7 +68,11 @@ static int map(struct dmar_domain *domain, vm_paddr_t start, vm_paddr_t size,
 		// map pages[last_i], ..., pages[i]
 		printf("[map] start %lx - size %lx\n", start + GMEM_PAGE_SIZE * last_i,
 			(i + 1 - last_i) * GMEM_PAGE_SIZE);
-		error = domain_map_buf_lockless(domain, start + GMEM_PAGE_SIZE * last_i,
+
+		// error = domain_map_buf_lockless(domain, start + GMEM_PAGE_SIZE * last_i,
+		// 	(i + 1 - last_i) * GMEM_PAGE_SIZE, VM_PAGE_TO_PHYS(pages[last_i]),
+		// 	DMAR_PTE_R | DMAR_PTE_W, GMEM_WAITOK);
+		error = domain_map_buf(domain, start + GMEM_PAGE_SIZE * last_i,
 			(i + 1 - last_i) * GMEM_PAGE_SIZE, VM_PAGE_TO_PHYS(pages[last_i]),
 			DMAR_PTE_R | DMAR_PTE_W, GMEM_WAITOK);
 
